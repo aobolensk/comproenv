715e80a43368dcea6f50e18635749f5754dca4ca
Arseniy Obolenskiy <gooddoog@student.su>
2019-04-23 11:03:12 +0300

    (#5) Implement YAMLParser types: Value, Mapping, Sequence

--- include/yaml_parser.h
+++ include/yaml_parser.h
@@ -17,6 +17,35 @@ class YAMLParser {
     YAMLParser(std::string file_name);
     YAMLParser(const YAMLParser &p);
     ~YAMLParser();
+    class Value;
+    using Sequence = std::vector <Value>;
+    class Mapping {
+     private:
+        std::map <std::string, Value> map_;
+     public:
+        bool hasKey(const std::string &name) const;
+        Value getValue(const std::string &name) const;
+        std::string getString(const std::string &name) const;
+        Mapping getMapping(const std::string &name) const;
+        Sequence getSequence(const std::string &name) const;
+    };
+    class Value {
+     private:
+        enum class Type {
+            String, Mapping, Sequence
+        };
+        Type type_;
+        std::string string_;
+        Mapping mapping_;
+        Sequence sequence_;
+     public:
+        Value(std::string &&str) : type_(Type::String), string_(std::move(str)) {}
+        Value(Mapping &&map) : type_(Type::Mapping), mapping_(std::move(map)) {}
+        Value(Sequence &&seq) : type_(Type::Sequence), sequence_(std::move(seq)) {}
+        const std::string &getString() const;
+        const Mapping &getMapping() const;
+        const Sequence &getSequence() const;
+    };
 };
 
 #endif  // INCLUDE_YAML_PARSER_H
--- src/yaml_parser.cpp
+++ src/yaml_parser.cpp
@@ -22,3 +22,53 @@ YAMLParser::YAMLParser(const YAMLParser &p) :
 YAMLParser::~YAMLParser() {
     yaml_parser_delete(&parser_);
 }
+
+bool YAMLParser::Mapping::hasKey(const std::string &name) const {
+    return map_.find(name) != map_.end();
+}
+
+const std::string &YAMLParser::Value::getString() const {
+    if (type_ != Type::String)
+        throw std::runtime_error("Value is not String type");
+    return string_;
+}
+
+const YAMLParser::Mapping &YAMLParser::Value::getMapping() const {
+    if (type_ != Type::Mapping)
+        throw std::runtime_error("Value is not Mapping type");
+    return mapping_;
+}
+
+const YAMLParser::Sequence &YAMLParser::Value::getSequence() const {
+    if (type_ != Type::Sequence)
+        throw std::runtime_error("Value is not Sequence type");
+    return sequence_;
+}
+
+YAMLParser::Value YAMLParser::Mapping::getValue(const std::string &name) const {
+    auto res = map_.find(name);
+    if (res == map_.end())
+        throw std::runtime_error("Wrong key");
+    return (*res).second;
+}
+
+std::string YAMLParser::Mapping::getString(const std::string &name) const {
+    auto res = map_.find(name);
+    if (res == map_.end())
+        throw std::runtime_error("Wrong key");
+    return (*res).second.getString();
+}
+
+YAMLParser::Mapping YAMLParser::Mapping::getMapping(const std::string &name) const {
+    auto res = map_.find(name);
+    if (res == map_.end())
+        throw std::runtime_error("Wrong key");
+    return (*res).second.getMapping();
+}
+
+YAMLParser::Sequence YAMLParser::Mapping::getSequence(const std::string &name) const {
+    auto res = map_.find(name);
+    if (res == map_.end())
+        throw std::runtime_error("Wrong key");
+    return (*res).second.getSequence();
+}
