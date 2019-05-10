d6e10ee208b8addcf5496d7b436faa8c9a3126ac
Arseniy Obolenskiy <gooddoog@student.su>
2019-05-10 10:38:26 +0300

    (#69) Add parser for timus

--- scripts/parser.py
+++ scripts/parser.py
@@ -52,7 +52,25 @@ def run():
     elif (link.startswith("http://acm.timus.ru/") or
         link.startswith("https://acm.timus.ru/") or
         link.startswith("https://timus.online/")):
-        pass
+        page = requests.get(link).text
+        soup = BeautifulSoup(page, "html.parser")
+        tables = soup.find_all("table", class_="sample")
+        for table in tables:
+            rows = table.find_all("tr")
+            cells = rows[0].find_all("th")
+            check = 2
+            for cell in cells:
+                if cell.contents[0].find("input") != -1:
+                    check -= 1
+                elif cell.contents[0].find("output") != -1:
+                    check -= 1
+            if (check == 0):
+                for i in range(1, len(rows)):
+                    cells = rows[i].find_all("td")
+                    with open(os.path.join(path, "timus_sample_" + str(i)) + ".in", "w") as f:
+                        f.write(cells[0].get_text())
+                    with open(os.path.join(path, "timus_sample_" + str(i)) + ".out", "w") as f:
+                        f.write(cells[1].get_text())
     else:
         print("Couldn't parse contents of this page")
         exit(-1)
